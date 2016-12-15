daea44e5a6df485143db10ae01222425df49e06f
Morgan Flyer <MorganFlyer@Stevens-MacBook-Pro.local>
2016-12-14 20:38:39 -0500

    my submission

--- FinalWebSite/functional_tests.py
+++ FinalWebSite/functional_tests.py
@@ -17,15 +17,27 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
-		self.browser.quit()
+			self.browser.quit()
 
 	def test_home_page(self):
 		self.browser.get('http://localhost:8000/index.html')
-
 		# when we are on the home page, the page title says "The Mandelbrot Set"
 		self.assertIn("The Mandelbrot Set",self.browser.title)
+		a=self.browser.find_element_by_tag_name('h1')
+		a=self.browser.find_element_by_tag_name('img')
+		self.assertIn('M.jpg',a.get_attribute('src'))
+		
+		
+		a=self.browser.find_element_by_id('code')
+		a.click()
+		h=self.browser.find_element_by_tag_name('h1')
+		self.assertIn('The Code',self.browser.title)
+		m=self.browser.find_element_by_tag_name('img')
+		self.assertIn('mbrot.png',m.get_attribute('src'))
+		a=self.browser.find_element_by_id ('index')
+		a.click()
+		
 		
 
-if __name__=="__main__":
-		unittest.main(warnings="ignore")
-
+if __name__=="__main__":		
+	unittest.main(warnings="ignore")
